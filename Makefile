2b0ce9f077dd2abaff1a9c1fc6f1c9876086d30a
LXW <lixiaowei.ying@gmail.com>
2016-05-12 17:19:05 -0700

    完善代码，修复BUG，准备提交代码

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 cc=g++
-CXXFLAGS = -I/usr/local/include/libxml2 
+CXXFLAGS = -I/usr/include/libxml2 
 LIBS = -lpthread -lrt -lxml2 -lnetsnmp
 
 obj = handle_information.o utils.o ctdb_ip.o
 
 collect_send: $(obj)
-	$(cc) -o collect_send $(obj) $(LIBS)
+	$(cc) -o collect_send -g $(obj) $(LIBS)
 
 .PHONY:clean
 
--- ctdb_ip.h
+++ ctdb_ip.h
@@ -3,6 +3,18 @@
 
 #include <semaphore.h>
 #include <sys/types.h>
+#include <vector>
+#include <string>
+
+#define		MAX_DEV_VIRIP	1024
+#define		SEM_NAME			"sem_dev_virip"
+#define		SHM_NAME			"shm_dev_virip"
+
+struct Dev_Virip{
+	char devip[16];
+	int num;
+	char virip[1024][16];
+};
 
 /*
  *	根据指定的物理ip获取他的所有虚拟ip
@@ -14,7 +26,7 @@
  *	返回值：
  *		成功返回虚拟ip的格式，失败返回-1
  * */
-int virip_by_devip(const char *devip,char virip[][16],size_t size);
+int virip_by_devip(const char *devip,char virip[][16]);
 
 /*
  *	根据指定的虚拟IP，获取对应的唯一的物理IP
@@ -24,19 +36,34 @@ int virip_by_devip(const char *devip,char virip[][16],size_t size);
  *		length，指定devip的长度，
  *	成功返回物理IP的指针，失败返回NULL
  * */
-char* devip_by_virip(const char *virip, char *devip,size_t length);
+char* devip_by_virip(const char *virip, char *devip);
 
 /*
- *	初始化信号量
+ *	初始化信号量和共享内存
+ *	返回值：
+ *		成功返回0，失败返回-1
+ * */
+int shm_init(void);
+
+
+/*
+ *	打开信号量
  *	返回值：
  *		成功返回指针sem，失败返回NULL
  * */
-sem_t *init_sem(void);
+sem_t *semopen(void);
+
+/*
+ *	打开共享内存
+ *	返回值：
+ *		成功返回指针shmptr，失败返回NULL
+ * */
+char *shmopen(void);
 
 /*
  *	根据有名信号量将获取到的物理ip和虚拟IP放入全局变量dev_virip中
  *	成功返回0，失败返回-1
  * */
-int update_dev_virip(sem_t *sem);
+int update_dev_virip(char *shmptr,sem_t *sem);
 
 #endif		//__CTDB_IP_H__
